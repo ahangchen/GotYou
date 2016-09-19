8800b8e913970c831630d62083ee24d2bb7825b8
ahangchen <cweihang@foxmail.com>
2016-09-19 18:55:22 +0800

    增加名字中的空格识别

--- app/util/request.py
+++ app/util/request.py
@@ -1,5 +1,6 @@
 def invalid_stu_name(name):
-    return not name or any(char.isdigit() for char in name)
+    return not name or any(char.isdigit() or char == ' ' for char in name)
+
 
 def invalid_paper_name(name):
     return not name
